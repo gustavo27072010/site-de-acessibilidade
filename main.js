4b9d218920f79e1157e3971027d8dc5fe0b86ebc
gustavo27072010 <gustavo.gomes.santos27@escola.pr.gov.br>
2026-08-14 07:58:25 -0300

    g

--- main.js
+++ main.js
@@ -210,3 +210,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+function handleCredentialResponse(response) {
+  const data = parseJwt(response.credential);
+  alert(`Olá, ${data.name}! Login com a conta Google realizado com sucesso.`);
+}
+
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
